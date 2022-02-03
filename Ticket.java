47a6dda3970ecdad283afd18f5360c8c00da801c
igSPrad <96576039+igSPrad@users.noreply.github.com>
2022-02-03 21:30:49 +0530

    Add files via upload

---
+++ Ticket.java
@@ -0,0 +1,18 @@
+public class Ticket  {
+    private  String arrival;
+    private  String Departure;
+    String getArrival(){
+        return arrival;
+    }
+    public void setArrival(String arrival){
+        this.arrival=arrival;
+    }
+
+    String getDeparture(){
+        return Departure;
+    }
+    public void setDeparture(String Departure){
+        this.Departure=Departure;
+    }
+
+}
---
+++ TouristTicket.java
@@ -0,0 +1,40 @@
+public class TouristTicket extends Ticket {
+    private String PnrNum;
+    private String Seat_no;
+    private String Ac_Details;
+    private String Flight_time;
+    private int TicketFare;
+    String getPnrNum () {
+        return PnrNum;
+    }
+    public void setPnrNum (String PnrNum){
+        this.PnrNum = PnrNum;
+    }
+
+    String getSeat_no(){
+        return Seat_no;
+    }
+    public void setSeat_no(String Seat_no){
+        this.Seat_no=Seat_no;
+    }
+
+    String getAc_Details(){
+        return Ac_Details;
+    }
+    public void Ac_Details(String Ac_Details){
+        this.Ac_Details=Ac_Details;
+    }
+
+    String getFlight_time(){
+        return Flight_time;
+    }
+    public void setFlight_time(String Flight_time ){
+        this.Flight_time=Flight_time;
+    }
+    int getTicketFare(){
+        return TicketFare;
+    }
+    public void setTicketFare(int TicketFare){
+        this.TicketFare=TicketFare;
+    }
+}
